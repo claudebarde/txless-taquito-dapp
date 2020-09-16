e5c4e82ae27a02157a34e427dcff4113f461a500
Claude Barde <25201109+claudebarde@users.noreply.github.com>
2020-09-16 14:01:40 +0100

    Added buttons to let user chose the amount to increment/decrement, started code to forge operations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from "react";
+import {
+  RpcClient,
+  OperationContentsTransaction,
+  OpKind,
+  MichelsonV1Expression
+} from "@taquito/rpc";
 import "./App.css";
+import { TezBridgeSigner } from "@taquito/tezbridge-signer";
 
 const App = () => {
   const [storage, setStorage] = useState(0);
   const [loadingIncrement, setLoadingIncrement] = useState(false);
   const [loadingDecrement, setLoadingDecrement] = useState(false);
   const [opHash, setOpHash] = useState<string>();
+  const [incrementValue, setIncrementValue] = useState<number>(0);
+  const [decrementValue, setDecrementValue] = useState<number>(0);
+  const [client, setClient] = useState<RpcClient>();
+  const [pkh, setPkh] = useState<string>();
+  const [signer, setSigner] = useState();
+
+  const contractAddress = "KT1Pdsb8cUZkXGxVaXCzo9DntriCEYdG9gWT";
 
   const increment = async () => {
     // increments the storage by 1
@@ -52,8 +66,38 @@ const App = () => {
     }
   };
 
+  const signIncrement = async () => {
+    console.log(signer);
+    const txData: OperationContentsTransaction = {
+      kind: OpKind.TRANSACTION,
+      source: pkh,
+      fee: "300000",
+      counter: "",
+      gas_limit: "219104",
+      storage_limit: "0",
+      amount: "0",
+      destination: contractAddress,
+      parameters: {
+        entrypoint: "increment",
+        value: `{"prim": "int", "args": [${incrementValue}}]` as MichelsonV1Expression
+      }
+    };
+    //const op = await client.forgeOperations(txData);
+    const op = await client.preapplyOperations([txData]);
+    console.log(op);
+  };
+
   useEffect(() => {
     (async () => {
+      // sets up the RPC client
+      const client = new RpcClient("https://testnet-tezos.giganode.io");
+      setClient(client);
+      // sets up the signer
+      const signer = new TezBridgeSigner();
+      const pkh = await signer.publicKeyHash();
+      setSigner(signer);
+      setPkh(pkh);
+      // fetches the storage
       const res = await fetch("/storage");
       const data = await res.json();
       const storage = JSON.parse(data);
@@ -66,8 +110,9 @@ const App = () => {
   return (
     <main>
       <div className="app">
-        <h2>Walletless Taquito Dapp</h2>
+        <h2>Transactionless Taquito Dapp</h2>
         <h4>Current storage: {storage}</h4>
+        <div>{pkh ? <p>{pkh}</p> : <p></p>}</div>
         <div>
           {opHash ? (
             <p>
@@ -105,6 +150,42 @@ const App = () => {
             {loadingDecrement && <div className="icon loader"></div>}Decrease by
             1
           </button>
+        </div>
+        <div className="custom-functions">
+          <div>
+            <span>Increment by:</span>
+            <input
+              type="text"
+              value={incrementValue}
+              onChange={e => {
+                const val = e.target.value;
+                if (!isNaN(+val)) {
+                  setIncrementValue(+val.slice(0, 3));
+                } else {
+                  setIncrementValue(0);
+                }
+              }}
+            />
+            <button className="button" onClick={signIncrement}>
+              Confirm
+            </button>
+          </div>
+          <div>
+            <span>Decrement by:</span>
+            <input
+              type="text"
+              value={decrementValue}
+              onChange={e => {
+                const val = e.target.value;
+                if (!isNaN(+val)) {
+                  setDecrementValue(+val.slice(0, 3));
+                } else {
+                  setDecrementValue(0);
+                }
+              }}
+            />
+            <button className="button">Confirm</button>
+          </div>
         </div>
       </div>
     </main>
